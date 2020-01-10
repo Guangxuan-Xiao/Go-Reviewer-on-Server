28174e8a29fa1c197460fd9e2d8adfee1b253e7b
Guangxuan-Xiao <xiaoguangxuan@outlook.com>
2020-01-11 00:44:11 +0800

    socket

---
+++ socket/client.py
@@ -0,0 +1,26 @@
+# client.py
+
+import socket  # Import socket module
+
+s = socket.socket()  # Create a socket object
+host = "166.111.5.105"  # Get local machine name
+port = 60000  # Reserve a port for your service.
+
+s.connect((host, port))
+s.send("Hello server!".encode("utf-8"))
+
+with open("received_file", "wb") as f:
+    print("file opened")
+    while True:
+        print("receiving data...")
+        data = s.recv(1024)
+        print("data=%s", (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
+
+print("Successfully get the file")
+s.close()
+print("connection closed")
+
---
+++ socket/server.py
@@ -0,0 +1,30 @@
+# server.py
+
+import socket  # Import socket module
+
+port = 60000  # Reserve a port for your service.
+s = socket.socket()  # Create a socket object
+host = socket.gethostname()  # Get local machine name
+print("Host:", host)
+s.bind((host, port))  # Bind to the port
+s.listen(5)  # Now wait for client connection.
+
+print("Server listening....")
+
+while True:
+    conn, addr = s.accept()  # Establish connection with client.
+    print("Got connection from", addr)
+    data = conn.recv(1024)
+    print("Server received: ", data.decode("utf-8"))
+
+    filename = "mytext.txt"
+    with open(filename, "wb+") as f:
+        l = f.read(1024)
+        while l:
+            conn.send(l)
+            print("Sent ", repr(l))
+            l = f.read(1024)
+
+    print("Done sending")
+    conn.send("Thank you for connecting".encode("utf-8"))
+    conn.close()
